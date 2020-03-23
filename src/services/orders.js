a437e9ae579015bfbfe9ae2545d24f0b1e9f009c
brownguo <yexuan.guo@codedance.cn>
2020-03-23 17:38:20 +0800

    auth

--- src/services/orders.js
+++ src/services/orders.js
@@ -4,7 +4,7 @@ import request from '@/utils/request';
 const rootPath='/v1';
 
 export async function queryRule(params) {
-  return request(`${rootPath}/orders/list/get?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyIwIjp7Imp0aSI6IndlaXlpaWQiLCJpc3MiOiJodHRwOlwvXC90ZXN0Lmp3dC5jb20iLCJhdWQiOiJodHRwOlwvXC90ZXN0Lmp3dC5jb20iLCJzdWIiOiJodHRwOlwvXC90ZXN0Lmp3dC5jb20iLCJpYXQiOjE1ODQ5NDU5NTIsIm5iZiI6MTU4NDk0NTk1MiwiZGF0YSI6W3siRmlkIjoiMiIsIkZhY2NvdW50IjoiYWRtaW4iLCJGbG9naW5Db3VudCI6IjU1MiIsIkZuaWNrbmFtZSI6Ilx1OThjZVx1NmUwNVx1NjI2YyJ9XX0sInNjb3BlcyI6IkFjY2VzcyIsImV4cCI6MTU4NDk0OTU1Mn0.NUuksnhCKffWMJALhgeUD0y3SYCJiIFVVfM5xbqImdI&id=${stringify(params)}`);
+  return request(`${rootPath}/orders/list/get?${stringify(params)}`);
 }
 
 export async function removeRule(params) {
--- src/utils/authority.js
+++ src/utils/authority.js
@@ -2,7 +2,7 @@
 export function getAuthority(str) {
   // return localStorage.getItem('antd-pro-authority') || ['admin', 'user'];
   const authorityString =
-    typeof str === 'undefined' ? localStorage.getItem('antd-pro-authority') : str;
+    typeof str === 'undefined' ? localStorage.getItem('crm-admin-authority-key') : str;
   // authorityString could be admin, "admin", ["admin"]
   let authority;
   try {
@@ -21,5 +21,5 @@ export function getAuthority(str) {
 }
 export function setAuthority(authority) {
   const proAuthority = typeof authority === 'string' ? [authority] : authority;
-  return localStorage.setItem('antd-pro-authority', JSON.stringify(proAuthority));
+  return localStorage.setItem('crm-admin-authority-key', JSON.stringify(proAuthority));
 }
