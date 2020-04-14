9870be0fcc11d054f5bb109719592c2e83d00422
brownguo <yexuan.guo@codedance.cn>
2020-04-14 15:08:35 +0800

    usersearch

--- src/pages/Configure/Managers.js
+++ src/pages/Configure/Managers.js
@@ -1,121 +1,19 @@
 import React, { PureComponent } from 'react';
-import { Card, Table, Alert, Divider, Menu, Dropdown, Icon, Button,Tabs } from 'antd';
+import { Card, Icon, Tabs } from 'antd';
 import PageHeaderWrapper from '@/components/PageHeaderWrapper';
-import { connect } from 'dva';
 import CreateUser from './CreateUser'
+import UserList from './UserList';
 
 const TabPane = Tabs.TabPane;
-/* eslint react/no-multi-comp:0 */
-@connect(({ configure, loading }) => ({
-  configure,
-  loading: loading.models.configure,
-}))
+
 class Managers extends PureComponent {
-  columns = [
-    {
-      title: '用户ID',
-      dataIndex: 'id',
-      render: text => <a href="#">{text}</a>,
-    },
-    {
-      title: '姓名',
-      className: 'column-money',
-      dataIndex: 'name',
-    },
-    {
-      title: '性别',
-      dataIndex: 'mobile_no',
-    },
-    {
-      title: '手机号',
-      dataIndex: 'callNo',
-    },
-    {
-      title: '邮箱',
-      dataIndex: 'status',
-    },
-    {
-      title: '注册时间',
-      dataIndex: 'create_at',
-    },
-    {
-      title: '操作',
-      render: (e, record) => (
-        <div>
-          <Button type="primary" size="small">
-            编辑
-          </Button>
-          <Divider type="vertical" />
-          <Dropdown overlay={() => this.menu(record)}>
-            <Button type="primary" ghost size="small">
-              更多
-              <Icon type="down" />
-            </Button>
-          </Dropdown>
-        </div>
-      ),
-    },
-  ];
-
-  componentDidMount() {
-    const { dispatch } = this.props;
-    dispatch({
-      type: 'configure/fetch',
-    });
-  }
-
-  menu = record => (
-    <Menu
-      onClick={key => {
-        console.log(key, record);
-      }}
-    >
-      <Menu.Item key="1">
-        <a href="#">删 除</a>
-      </Menu.Item>
-
-      <Menu.Item key="2">
-        <a href="#">分配角色</a>
-      </Menu.Item>
-
-      <Menu.Item key="3">
-        <a href="#">分配权限</a>
-      </Menu.Item>
-    </Menu>
-  );
-
   render() {
-    const {
-      loading,
-      configure: {
-        data: { data },
-      },
-    } = this.props;
-    const { list = [], pagination } = data;
-
     return (
       <PageHeaderWrapper title="管理员列表">
         <Card bordered={false}>
           <Tabs defaultActiveKey="1">
             <TabPane tab={<span><Icon type="smile-o" />用户列表</span>} key="1">
-              <Alert
-                message={
-                  <span>
-                    系统检索出总数据 <a href="#">{pagination.pageSize}</a> 条
-                  </span>
-                }
-                type="info"
-                showIcon
-                closeText="X"
-                style={{ marginBottom: '1%' }}
-              />
-              <Table
-                dataSource={list}
-                columns={this.columns}
-                rowKey="id"
-                loading={loading}
-                pagination={pagination}
-              />
+              <UserList />
             </TabPane>
             <TabPane tab={<span><Icon type="user-add" />新建用户</span>} key="2">
               <CreateUser />
---
+++ src/pages/Configure/UserList.js
@@ -0,0 +1,116 @@
+import React, { PureComponent } from 'react';
+import {Table, Alert, Divider, Menu, Dropdown, Icon, Button,Tabs } from 'antd';
+import { connect } from 'dva';
+
+import UserSearch from './UserSearch';
+
+/* eslint react/no-multi-comp:0 */
+@connect(({ configure, loading }) => ({
+  configure,
+  loading: loading.models.configure,
+}))
+
+class UserList extends PureComponent{
+  columns = [
+    {
+      title: '用户ID',
+      dataIndex: 'id',
+      render: text => <a href="#">{text}</a>,
+    },
+    {
+      title: '姓名',
+      className: 'column-money',
+      dataIndex: 'name',
+    },
+    {
+      title: '性别',
+      dataIndex: 'mobile_no',
+    },
+    {
+      title: '手机号',
+      dataIndex: 'callNo',
+    },
+    {
+      title: '邮箱',
+      dataIndex: 'status',
+    },
+    {
+      title: '注册时间',
+      dataIndex: 'create_at',
+    },
+    {
+      title: '操作',
+      render: (e, record) => (
+        <div>
+          <Button type="primary" size="small">
+            编辑
+          </Button>
+          <Divider type="vertical" />
+          <Dropdown overlay={() => this.menu(record)}>
+            <Button type="primary" ghost size="small">
+              更多
+              <Icon type="down" />
+            </Button>
+          </Dropdown>
+        </div>
+      ),
+    },
+  ];
+
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'configure/fetch',
+    });
+  }
+
+  menu = record => (
+    <Menu
+      onClick={key => {
+        console.log(key, record);
+      }}
+    >
+      <Menu.Item key="1">
+        <a href="#">删 除</a>
+      </Menu.Item>
+
+      <Menu.Item key="2">
+        <a href="#">分配角色</a>
+      </Menu.Item>
+
+      <Menu.Item key="3">
+        <a href="#">分配权限</a>
+      </Menu.Item>
+    </Menu>
+  );
+
+  render() {
+    const {
+      loading,
+      configure: {
+        data: { data },
+      },
+    } = this.props;
+    const { list = [], pagination } = data;
+    return(
+      <div style={{paddingRight: '1%', paddingLeft: '1%'}}>
+        <UserSearch />
+        <Alert
+          message={<span>系统检索出总数据 <a href="#">{pagination.pageSize}</a>条</span>}
+          type="info"
+          showIcon
+          closeText="X"
+          style={{ marginBottom: '1%' }}
+        />
+        <Table
+          dataSource={list}
+          columns={this.columns}
+          rowKey="id"
+          loading={loading}
+          pagination={pagination}
+        />
+      </div>
+    )
+  }
+}
+export default UserList;
